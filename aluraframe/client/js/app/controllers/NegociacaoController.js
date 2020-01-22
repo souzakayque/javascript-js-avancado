861e4e1a63843d6a6e737b76456b31e8ff5735dc
souzakayque <52177811+pereirakayque@users.noreply.github.com>
2020-01-22 20:48:37 -0300

    incluindo armadilha

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -7,7 +7,9 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        this._listaNegociacoes = new ListaNegociacoes();
+        this._listaNegociacoes = new ListaNegociacoes(model => 
+            this._negociacoesView.update(model));
+
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
 
         this._negociacoesView.update(this._listaNegociacoes);
@@ -28,6 +30,13 @@ class NegociacaoController {
         this._limpaFormulario();
         
         console.log(this._listaNegociacoes.Negociacoes);
+    }
+
+    apaga() {
+        this._listaNegociacoes.esvazia();
+        this._negociacoesView.update(this._listaNegociacoes);
+        this._mensagem.texto = 'Negociações apagadas com sucesso';
+        this._mensagemView.update(this._mensagem);
     }
 
     _criaNegociacao() {
--- aluraframe/client/js/app/models/ListaNegociacoes.js
+++ aluraframe/client/js/app/models/ListaNegociacoes.js
@@ -1,15 +1,23 @@
 class ListaNegociacoes {
 
-    constructor() {
+    constructor(context, armadilha) {
         this._negociacoes = [];
+        this._armadilha = armadilha;
+        this._context = context;
     }
 
     adiciona(negociacao) {
         this._negociacoes.push(negociacao);
+        this._armadilha(this);
     }
 
     get negociacoes() {
         return [].concat(this._negociacoes);
+    }
+
+    esvazia(){
+        this._negociacoes = [];
+        this._armadilha(this);    
     }
 
 }
