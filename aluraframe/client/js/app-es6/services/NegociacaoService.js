bad03b38ee3373627a6c82473281da2549fc41fd
souzakayque <52177811+pereirakayque@users.noreply.github.com>
2020-03-15 15:57:44 -0300

    trabalhando com babel e Promise Design Parttner

---
+++ aluraframe/client/js/app-es6/services/NegociacaoService.js
@@ -0,0 +1,117 @@
+class NegociacaoService {
+
+    constructor() {
+        this._http = new HttpService();
+    }
+
+    obterNegociacoesDaSemana() {
+
+        return new Promise((resolve, reject) => {                       
+            
+            this._http
+                .get('negociacoes/semana')
+                .then(negociacoes => {
+                    resolve(negociacoes.map(objeto => 
+                        new Negociacao (
+                            new Date(objeto.data),
+                            objeto.quantidade,
+                            objeto.valor)))
+                        })
+                .catch(erro => {
+                    console.log(erro);
+                    reject('Não foi possível obter as negociações da semana');
+                })
+        });
+    }
+
+    obterNegociacoesDaSemanaAnterior() {
+
+        return new Promise((resolve, reject) => {                       
+            
+            this._http
+                .get('negociacoes/anterior')
+                .then(negociacoes => {
+                    resolve(negociacoes.map(objeto => 
+                        new Negociacao (
+                            new Date(objeto.data),
+                            objeto.quantidade,
+                            objeto.valor)))
+                        })
+                .catch(erro => {
+                    console.log(erro);
+                    reject('Não foi possível obter as negociações da semana anterior');
+                })
+        });
+    }
+
+    obterNegociacoesDaSemanaRetrasada() {
+
+        return new Promise((resolve, reject) => {                       
+            
+            this._http
+                .get('negociacoes/retrasada')
+                .then(negociacoes => {
+                    resolve(negociacoes.map(objeto => 
+                        new Negociacao (
+                            new Date(objeto.data),
+                            objeto.quantidade,
+                            objeto.valor)))
+                        })
+                .catch(erro => {
+                    console.log(erro);
+                    reject('Não foi possível obter as negociações da semana retrasada');
+                })
+        });
+    }
+
+    cadastra(negociacao) {
+        
+        return ConnectionFactory
+            .getConnection()
+            .then(connection => new NegociacaoDao(connection))
+            .then(dao => dao.listaTodos())
+            .catch((erro) => {
+                console.log(erro);
+                throw new Error('Não foi possível adicionar a negociação');
+            });
+    }
+
+    lista() {
+        return ConnectionFactory
+            .getConnection()
+            .then(connection => new NegociacaoDao(connection))
+            .then(dao => dao.adiciona(negociacao))
+            .catch((erro) => {
+                console.log(erro);
+                throw new Error('Não foi possível obter as negociações');
+            });
+    }
+
+    apaga() {
+
+        return ConnectionFactory
+            .getConnection()
+            .then(connection => new NegociacaoDao(connection))
+            .then(dao => dao.apagaTodos())
+            .then(() => 'Negociacções apagadas com sucesso')
+            .catch(erro => {
+                console.log(erro);
+                throw new Error('Não foi possível apagar as negociações')
+            })
+    }
+
+    importa(listaAtual) {
+        
+        return this.obterNegociacoesDaSemana()
+            .then(negociacoes =>
+                negociacoes.filter(negociacao =>
+                    !listaAtual.some(negociacaoExistente =>
+                        JSON.stringify(negociacao) == JSON.stringify(negociacaoExistente)))
+            )
+            .catch(erro => {
+                console.log(erro);
+                throw new Error('Não foi possível buscar negociações para importar');
+            })
+    }
+    
+}
