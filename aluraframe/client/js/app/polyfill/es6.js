465fc1ab3bdc8a5d541c8d9362f99a930422895c
souzakayque <52177811+pereirakayque@users.noreply.github.com>
2020-03-15 16:02:56 -0300

    first build

---
+++ aluraframe/client/js/app/polyfill/es6.js
@@ -0,0 +1,8 @@
+'use strict';
+
+if (!Array.prototype.includes) {
+    console.log('Polyfill para Array.includes aplicado.');
+    Array.prototype.includes = function (elemento) {
+        return this.indexOf(elemento) != -1;
+    };
+}
